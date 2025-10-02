3817062b453d4a1b8488f81fab2ee491aba9f9a2
DustinJoosen <dustinjoosen2003@gmail.com>
2025-10-02 13:11:26 +0200

    Apply feedback

--- BAI1/BAI1/BAI_Afteken1.cs
+++ BAI1/BAI1/BAI_Afteken1.cs
@@ -17,30 +17,53 @@ namespace BAI
         // ***************
         public static UInt64 Opg1aDecodeBase27(string base27getal)
         {
+            /* Zuja: voeg nog wat nuttige commentaar toe, 
+            bijv. variabele uitleg, wat de loop doet, etc. */
+
             // *** IMPLEMENTATION HERE *** //
             UInt64 total = 0;
+
+            // Contains the current exponent value. Each iteration it multiplies itself by 27.
             UInt64 power = 1;
 
+            // You walk from right to left, so reverse the base27getal.
             var reverse = base27getal.Reverse();
 
             foreach (char c in reverse)
             {
+                // If the char is not part of BASE27, throw an exception.
+                if (!BASE27CIJFERS.Contains(c))
+                    throw new ArgumentException($"Character '{c}' is not able to be decoded.");
+
+                // Get the base-10 representation of the character.
                 UInt64 idx = (UInt64)BASE27CIJFERS.IndexOf(c);
 
                 total += idx * power;
+
+                // Essentially throws the exponent up. e.g. from 27^1 to 27^2.
                 power *= 27;
             }
             return total;
         }
         public static string Opg1bEncodeBase27(UInt64 base10getal)
         {
+            /* Zuja: voeg nog wat nuttige commentaar toe, 
+            bijv. variabele uitleg, wat de loop doet, etc. */
+
             // *** IMPLEMENTATION HERE *** //
             string result = string.Empty;
             UInt64 div = base10getal;
+
+            /* Always needs to run at least once, to prevent errors on a '-'.
+             * You grab the character that the number would collerate to (through modulo), 
+             * and prepend it to the output string. Then you recalculate the div.
+             * This continues until the remaining div is zero. */
             do
             {
                 var new_char = BASE27CIJFERS[(int)(div % 27)];
-                result = new_char.ToString() + result;
+                
+                // Zuja: Is de ToString hier nodig?
+                result = new_char + result;
                 div = div / 27;
             } while (div != 0);
 
@@ -67,6 +90,7 @@ namespace BAI
             while (woordstack.Count > 0)
             {
                 var woord = woordstack.Pop();
+                // Zuja: mooi.
                 if (woord < MAX_THREE_DIGITS)
                     queue.Enqueue(Opg1bEncodeBase27(woord));
             }
